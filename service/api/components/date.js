005955b6baf096390bcfc3ba38624b41ac925a67
xushaowei <xushaowei10077@autohome.com.cn>
2017-12-14 16:36:12 +0800

    修改ad.js

--- service/api/components/date.js
+++ service/api/components/date.js
@@ -7,11 +7,11 @@ Date.prototype.getYMD = function(time=new Date()){
 
 Date.prototype.getTimeQuantum = function(time=new Date()){
   let hour = time.getHours();
-  if( 0 <= hour && hour <= 8){
+  if( 0 <= hour && hour < 8){
     return 1
-  }else if( 8 < hour && hour <= 16 ){
+  }else if( 8 <= hour && hour < 16 ){
     return 2
-  }else if( 16 < hour && hour <= 24 ){
+  }else if( 16 <= hour && hour < 24 ){
     return 3
   }
 }
